aaffd74fc52dc13d6ffc1db3e7b55a9a53911398
Burhan Syed <burhan.syed@outlook.com>
2021-07-30 04:45:10 +0000

    iss 4 complete

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,20 +10,33 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIP('99.246.97.83', (error, data) => {
-  if (error) {
-    console.log("Didn't work!", error);
+// fetchCoordsByIP('99.246.97.83', (error, data) => {
+//   if (error) {
+//     console.log("Didn't work!", error);
+//   }
+//   
+//   console.log(latLongObj);
+// });
+
+// fetchISSFlyOverTimes({ latitude: '43.5639', longitude: '-79.7172' }, (error, data) => {
+//   if (error) console.log(error);
+
+//   console.log(data);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`)
   }
-  let latLongObj = {};
-  
-  latLongObj.latitude = data.latitude.toString();
-  latLongObj.longitude = data.longitude.toString();
-  console.log(latLongObj);
+}
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
-
-fetchISSFlyOverTimes({ latitude: '43.5639', longitude: '-79.7172' }, (error, data) => {
-  if (error) console.log(error);
-
-  console.log(data);
-});
-
